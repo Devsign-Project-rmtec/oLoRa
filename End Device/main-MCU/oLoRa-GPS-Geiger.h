86894eae9c8ae6b4c7e2431efb31502a53b014ed
_0x4d <vkdlsvnt@gmail.com>
2019-11-04 15:52:14 +0900

    PCB v19.2 update

---
+++ End Device/main-MCU/oLoRa-GPS-Geiger.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Arduino.h"
+
+#define DATA_GPS 0x01
+#define DATA_TIME 0x02
+#define DATA_USV 0x03
+
+class oLoRa_GPS_Geiger {
+private:
+
+public: 
+    oLoRa_GPS_Geiger() {
+        
+    }
+}
