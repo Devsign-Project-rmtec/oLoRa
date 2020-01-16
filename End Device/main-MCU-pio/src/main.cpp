e7d8b9d8f9988428c57b41d5b5818730d3649314
vkdlsvnt <vkdlsvnt@naver.com>
2020-01-16 22:13:14 +0900

    main-MCU PIO migration

---
+++ End Device/main-MCU-pio/src/main.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+
+#include "MPU6050.h"
+#include "DFRobot_SHT20.h"
+#include <Wire.h>
+
+#define LoRa Serial1
+
+MPU6050 imu; // I2C 0x68
+DFRobot_SHT20 sht20; // I2C 0x40
+
+void setup() {
+    Serial.begin(115200);
+    LoRa.begin(115200);
+    delay(1000);
+}
+
+void loop() {
+    int16_t x, y, z;
+    imu.getAcceleration(&x, &y, &z);
+    Serial.print(x);
+    Serial.print('\t');
+    Serial.print(y);
+    Serial.print('\t');
+    Serial.print(z);
+    Serial.println();
+    delay(10);
+
+    // float t, h;
+    // Serial.print("reading Temperature...");
+    // t = sht20.readTemperature();
+    // Serial.println("OK");
+    // h = sht20.readHumidity();
+    // Serial.print(t);
+    // Serial.print('\t');
+    // Serial.print(h);
+    // Serial.println();
+    // delay(100);
+}
